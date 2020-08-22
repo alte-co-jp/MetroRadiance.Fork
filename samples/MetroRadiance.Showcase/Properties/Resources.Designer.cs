e9d13608dab8a0e7d45d91b606c0a1e589ed0722
nishy2000 <github@nishy.net>
2020-08-22 16:58:22 +0900

    feat: Localize the MetroRadiance.Showcase app (English/Japanese)

--- samples/MetroRadiance.Showcase/Properties/Resources.Designer.cs
+++ samples/MetroRadiance.Showcase/Properties/Resources.Designer.cs
@@ -22,7 +22,7 @@ namespace MetroRadiance.Showcase.Properties {
     [global::System.CodeDom.Compiler.GeneratedCodeAttribute("System.Resources.Tools.StronglyTypedResourceBuilder", "16.0.0.0")]
     [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    internal class Resources {
+    public class Resources {
         
         private static global::System.Resources.ResourceManager resourceMan;
         
@@ -36,7 +36,7 @@ namespace MetroRadiance.Showcase.Properties {
         ///   Returns the cached ResourceManager instance used by this class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Resources.ResourceManager ResourceManager {
+        public static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
                     global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("MetroRadiance.Showcase.Properties.Resources", typeof(Resources).Assembly);
@@ -51,12 +51,336 @@ namespace MetroRadiance.Showcase.Properties {
         ///   resource lookups using this strongly typed resource class.
         /// </summary>
         [global::System.ComponentModel.EditorBrowsableAttribute(global::System.ComponentModel.EditorBrowsableState.Advanced)]
-        internal static global::System.Globalization.CultureInfo Culture {
+        public static global::System.Globalization.CultureInfo Culture {
             get {
                 return resourceCulture;
             }
             set {
                 resourceCulture = value;
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to HSL color model &amp; Luminosity.
+        /// </summary>
+        public static string ColorModel_TitleHSLColorModel {
+            get {
+                return ResourceManager.GetString("ColorModel_TitleHSLColorModel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to HSV color model &amp; Luminosity.
+        /// </summary>
+        public static string ColorModel_TitleHSVColorModel {
+            get {
+                return ResourceManager.GetString("ColorModel_TitleHSVColorModel", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Hue.
+        /// </summary>
+        public static string ColorModel_TitleHue {
+            get {
+                return ResourceManager.GetString("ColorModel_TitleHue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Lightness.
+        /// </summary>
+        public static string ColorModel_TitleLightness {
+            get {
+                return ResourceManager.GetString("ColorModel_TitleLightness", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Saturation.
+        /// </summary>
+        public static string ColorModel_TitleSaturation {
+            get {
+                return ResourceManager.GetString("ColorModel_TitleSaturation", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Value.
+        /// </summary>
+        public static string ColorModel_TitleValue {
+            get {
+                return ResourceManager.GetString("ColorModel_TitleValue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Create metro:_AcrylicBlurWindow.
+        /// </summary>
+        public static string ControlSample_ButtonCreateAcrylicBlurWindow {
+            get {
+                return ResourceManager.GetString("ControlSample_ButtonCreateAcrylicBlurWindow", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Create metro:_BlurWindow.
+        /// </summary>
+        public static string ControlSample_ButtonCreateBlurWindow {
+            get {
+                return ResourceManager.GetString("ControlSample_ButtonCreateBlurWindow", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to UI Control samples.
+        /// </summary>
+        public static string ControlSample_GroupHeader {
+            get {
+                return ResourceManager.GetString("ControlSample_GroupHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Enable UI controls (IsEnabled = True).
+        /// </summary>
+        public static string ControlSample_IsEnabledUIControls {
+            get {
+                return ResourceManager.GetString("ControlSample_IsEnabledUIControls", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Check box.
+        /// </summary>
+        public static string ControlSample_TextCheckBox {
+            get {
+                return ResourceManager.GetString("ControlSample_TextCheckBox", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Radio button item 1.
+        /// </summary>
+        public static string ControlSample_TextRadioButtonItem1 {
+            get {
+                return ResourceManager.GetString("ControlSample_TextRadioButtonItem1", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Radio button item 2.
+        /// </summary>
+        public static string ControlSample_TextRadioButtonItem2 {
+            get {
+                return ResourceManager.GetString("ControlSample_TextRadioButtonItem2", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Radio button item 3.
+        /// </summary>
+        public static string ControlSample_TextRadioButtonItem3 {
+            get {
+                return ResourceManager.GetString("ControlSample_TextRadioButtonItem3", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Toggle button.
+        /// </summary>
+        public static string ControlSample_TextToggleButton {
+            get {
+                return ResourceManager.GetString("ControlSample_TextToggleButton", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Move your mouse pointer inside this pane in order to show tooltip.
+        /// </summary>
+        public static string ControlSample_TextTooltipDesc {
+            get {
+                return ResourceManager.GetString("ControlSample_TextTooltipDesc", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Tooltip sample text.
+        /// </summary>
+        public static string ControlSample_TextTooltipSamleText {
+            get {
+                return ResourceManager.GetString("ControlSample_TextTooltipSamleText", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Immersive color samples.
+        /// </summary>
+        public static string ImmersiveColor_TitleImmersiveColorSample {
+            get {
+                return ResourceManager.GetString("ImmersiveColor_TitleImmersiveColorSample", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to HSV / HSL / Luminosity.
+        /// </summary>
+        public static string TabItemHeader_ColorModelSupport {
+            get {
+                return ResourceManager.GetString("TabItemHeader_ColorModelSupport", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Controls.
+        /// </summary>
+        public static string TabItemHeader_Controls {
+            get {
+                return ResourceManager.GetString("TabItemHeader_Controls", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Immersive colors.
+        /// </summary>
+        public static string TabItemHeader_ImmersiveColors {
+            get {
+                return ResourceManager.GetString("TabItemHeader_ImmersiveColors", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Theme brushes.
+        /// </summary>
+        public static string TabItemHeader_ThemeBrushes {
+            get {
+                return ResourceManager.GetString("TabItemHeader_ThemeBrushes", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Blue.
+        /// </summary>
+        public static string ThemeAndAccent_AccentBlue {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_AccentBlue", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Orange.
+        /// </summary>
+        public static string ThemeAndAccent_AccentOrange {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_AccentOrange", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Purple.
+        /// </summary>
+        public static string ThemeAndAccent_AccentPurple {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_AccentPurple", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Red.
+        /// </summary>
+        public static string ThemeAndAccent_AccentRed {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_AccentRed", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Windows.
+        /// </summary>
+        public static string ThemeAndAccent_AccentWindows {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_AccentWindows", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Dark.
+        /// </summary>
+        public static string ThemeAndAccent_DarkMode {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_DarkMode", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Theme mode &amp; accent color.
+        /// </summary>
+        public static string ThemeAndAccent_GroupHeader {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_GroupHeader", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Light.
+        /// </summary>
+        public static string ThemeAndAccent_LightMode {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_LightMode", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Accent color:.
+        /// </summary>
+        public static string ThemeAndAccent_TitleAccentColor {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_TitleAccentColor", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to Theme mode: .
+        /// </summary>
+        public static string ThemeAndAccent_TitleThemeMode {
+            get {
+                return ResourceManager.GetString("ThemeAndAccent_TitleThemeMode", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to MetroRadiance border &amp; background brushes.
+        /// </summary>
+        public static string ThemeBrush_MetroRadianceBrush {
+            get {
+                return ResourceManager.GetString("ThemeBrush_MetroRadianceBrush", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to UWP compatible accent colors.
+        /// </summary>
+        public static string ThemeBrush_UWPAccentColors {
+            get {
+                return ResourceManager.GetString("ThemeBrush_UWPAccentColors", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to UWP compatible theme brushes.
+        /// </summary>
+        public static string ThemeBrush_UWPBrush {
+            get {
+                return ResourceManager.GetString("ThemeBrush_UWPBrush", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Looks up a localized string similar to UI Window samples.
+        /// </summary>
+        public static string WindowSample_GroupHeader {
+            get {
+                return ResourceManager.GetString("WindowSample_GroupHeader", resourceCulture);
             }
         }
     }
